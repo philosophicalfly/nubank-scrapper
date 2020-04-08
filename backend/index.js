75ed30c717ddca662d06ee83971619d480fd507f
Augusto Bardini <azbardini@gmail.com>
2020-04-08 13:43:49 -0300

    :sparkles: Add mongoose

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const routes = require('./src/routes');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://nuAdmin:nuAdmin@cluster0-rmrex.mongodb.net/wazev?retryWrites=true&w=majority', {
+   useNewUrlParser: true,   //Removing warnings
+   useUnifiedTopology: true //Removing warnings
+});
 
 app.use(cors(
   // {
