2b5bb4c1d8229de4d2ff58b751172bf23a467ee3
Augusto Bardini <azbardini@gmail.com>
2020-04-10 14:36:58 -0300

    :fire: Remove logs

--- backend/src/modules/CommonModule.js
+++ backend/src/modules/CommonModule.js
@@ -6,12 +6,7 @@ async function getPermissions (req) {
         try {
             const login = await decrypt(req.headers.login);
             const passwd = await decrypt(req.headers.passwd);
-            console.log('getPermissions -> login', login);
-            console.log('getPermissions -> context.login', context.login);
-            console.log('getPermissions -> passwd', passwd);
-            console.log('getPermissions -> context.passwd', context.passwd);
             if (context.login === login && context.passwd === passwd) {
-                console.log('has');
                 return true;
             }
         } catch (error) {
