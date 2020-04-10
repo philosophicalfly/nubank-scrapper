0161e76678824243ee4854e720c894d22a349428
Augusto Bardini <azbardini@gmail.com>
2020-04-10 15:38:42 -0300

    :lipstick: Run prettier in some files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Routes from './routes';
 
 function App() {
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import UserLogin from './UserLogin';
 import Dashboard from './Dashboard';
@@ -7,8 +7,8 @@ import Dashboard from './Dashboard';
 export default function Routes() {
   return (<BrowserRouter>
     <Switch>
-      <Route path="/" exact component={UserLogin}/>
-      <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/" exact component={UserLogin} />
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   </BrowserRouter>)
 }
